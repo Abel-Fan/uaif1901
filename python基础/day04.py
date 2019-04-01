c409cc085a2266d61d2da8f8dae47987372a7d4f
Abel-Fan <842615663@qq.com>
2019-04-01 17:41:33 +0800

    14

--- python基础/day04.py
+++ python基础/day04.py
@@ -210,31 +210,162 @@
 #
 # # __new__ 与 __init__ 关系
 
-import time
+# import time
+#
+# def sumtime(fn):
+#     def newFn(self,*args,**kwargs):
+#         start = time.time()
+#         fn(self,*args,**kwargs)
+#         end = time.time()
+#         print("%s你好帅"%self.name)
+#         print("总时间:%s"%(end-start))
+#     return newFn
+#
+# # 类装饰器
+# class P:
+#     def __init__(self,name):
+#         self.name= name
+#     @sumtime
+#     def run(self):
+#         time.sleep(1)
+#         print(self.name)
 
-def sumtime(fn):
-    def newFn(self,*args,**kwargs):
-        start = time.time()
-        fn(self,*args,**kwargs)
-        end = time.time()
-        print("%s你好帅"%self.name)
-        print("总时间:%s"%(end-start))
-    return newFn
+# # 实例方法装饰器
+#
+#
+# p = P("horns")
+# p.run()
+#
+
 
 # 类装饰器
-class P:
-    def __init__(self,name):
-        self.name= name
-    @sumtime
+#
+# def MyClass(obj):
+#     obj.age = 19   # 类变量
+#     obj.say=lambda cls:print(cls.age) # 类方法
+#     return obj
+# class A():
+#     def __init__(self):
+#         pass
+#     def run(self):
+#         print("run")
+# @MyClass
+# class P(A):
+#     def __init__(self,name):
+#         self.name = name
+#
+# p = P("小红")
+
+
+# 验证
+# isinstance(obj,class) 验证obj是否为class的实例
+# print(isinstance(p,P))
+# issubclass(子类,父类)
+# print(issubclass(A,P))
+# type() 查看类型
+# dir() 查看实例的属性和方法
+
+# 文件操作
+# 三个步骤
+# 1、打开文件
+# 2、读写内容
+# 3、关闭文件
+# open(file,mode) 打开文件
+"""
+file 文件路径
+mode 读取模式
+    r 读取内容
+    r+ 读写,从头覆盖旧内容，没有文件则报错
+    w  重新写入,没有文件会创建文件
+    w+ 读文件 + 重新写入,没有文件会创建文件 
+    a  追加 +没有文件会创建文件
+    a+ 读+追加
+    
+    rb、rb+、wb、wb+、ab、ab+  以二进制的方式处理
+    
+encoding: 字符编码
+errors  ：出错处理机制  strict(默认，报错) ignore(忽略)
+"""
+
+
+f1 = open("./day04.txt",'rb')
+f2 = open("./day0401.txt",'w',encoding="utf8")
+# 读
+# con = f.read()  # 全部输出
+# con = f.read(5)  # 读取5个字符
+# con = f.readline() # 读取第一行
+# con = f.readline(2) # 读取一行的前2个字符
+
+# while True:
+#     con = f1.readline()
+#     if not con:
+#         break
+#     print(con)
+
+
+# con = f.readlines()  #读取全部 以列表方式返回每行内容
+# print(con)
+
+# 写
+# f.write("abcde")  # 写入'abcde'
+# lins = f1.readlines()
+# print(lins)
+# f2.writelines(lins)
+# f1.close()
+# f2.close()
+
+# 操作指针
+# f1.seek(2,0) # 移动文件指针到 第六个字节
+# f1.seek(2,1) # 相对于文章尾
+# print(f1.read(1))
+# print(f1.tell())
+# f1.close()
+
+
+# import pickle
+# # pickle.dump(obj,file)  把obj存储到file文件中
+# # pickle.load(file)  把file文件中的数据进行读取
+#
+# f = open('day04.txt','rb')
+#
+# # obj = [1,2,3,4,5,6,7,8,9]
+# # pickle.dump(obj,f)
+# obj = pickle.load(f)
+# print(obj)
+# f.close()
+
+# 文件打开或读取时，可能会发生IOError,可能导致文件无法关闭。
+
+# try:      # 可能会错误的代码
+#     f = open("day04.txt",'r',encoding="utf8")
+#     con = f.read()
+#     print(con)
+# except:   # 出错处理程序
+#     print("文件出错了")
+# finally:  # 不管正确还是错误 总会执行
+#     f.close()
+
+# 重点
+# with 上下文管理器
+# 语法
+# with open('day04.txt','rb') as f:
+#     f.read()
+# 模拟上下文管理器
+class Simple:
+    def __enter__(self):
+        print("enter")
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('exit')
+        print("exc_type",exc_type)
+        print('exc_val',exc_val)
+        print('exc_tb',exc_tb)
+        return True
     def run(self):
-        time.sleep(1)
-        print(self.name)
+        print(1/0)
 
-# 实例方法装饰器
+def simple():
+    return Simple()
 
-
-p = P("horns")
-p.run()
-
-
-
+with simple() as f:
+    f.run()
