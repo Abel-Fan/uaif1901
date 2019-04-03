ab42928d09341e6aacf7a51bb049121a11a86f7c
Abel-Fan <842615663@qq.com>
2019-04-03 17:37:39 +0800

    19

--- 02python爬虫/day06.py
+++ 02python爬虫/day06.py
@@ -66,3 +66,78 @@ import time
 # b = Foo('xm')
 # a.say()
 # b.say()
+
+# 一、操作表格
+
+# import xlwt,pickle
+#
+# wb = xlwt.Workbook() # 创建表格对象
+# ws = wb.add_sheet("abc") #创建表
+#
+#
+# with open("top250.txt",'rb') as f:
+#     arr = pickle.load(f)
+#
+# index = 0
+# for arr2 in arr:
+#     for title,url in arr2:
+#         # # 序号
+#         ws.write(index,0,index+1)
+#         # title
+#         ws.write(index,1,title)
+#         # url
+#         ws.write(index,2,url)
+#         # print(index,title,url)
+#         index+=1
+#
+# wb.save("豆瓣电影.xls")
+
+# 爬取豆瓣
+
+from urllib.request import *
+import pickle,fake_useragent
+from lxml.etree import *
+
+with open('top250.txt','rb') as f:
+    arr = pickle.load(f)
+lists = []
+for arr1 in arr:
+    for title,url in arr1:
+        lists.append(url)
+
+ua = fake_useragent.UserAgent()
+header = {
+    'User-Agent':ua.random
+}
+def spider(url):
+    req = Request(url,headers=header)
+    with urlopen(req) as html:
+        text = html.read().decode()
+    doc = HTML(text)
+    # 导演
+    pl1 = "/".join(doc.xpath("//div[@id='info']/span[1]/span[@class='attrs']/a[1]/text()"))
+    # 编剧
+    pl2 = "".join(doc.xpath("//div[@id='info']/span[2]/span[@class='attrs']/a/text()"))
+    # 主演
+    pl3 = "/".join(doc.xpath("//div[@id='info']/span[3]/span[@class='attrs']/a/text()"))
+    # 类型
+    pl4 = "/".join(doc.xpath("//div[@id='info']/span[@property='v:genre']/text()"))
+    # 剧情简介
+    pl5 = doc.xpath("//span[@property='v:summary']/text()")[0].strip()
+
+    return {
+        'dir':pl1,
+        'edit':pl2,
+        'actor':pl3,
+        'type':pl4,
+        'dis':pl5
+    }
+
+
+data = []
+for url in lists:
+    res = spider(url)
+    print(res)
+
+
+
