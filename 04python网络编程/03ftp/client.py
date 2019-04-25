74c2d2e479dfc1b0e4541978577a49e23aca7b76
Abel-Fan <842615663@qq.com>
2019-04-25 14:26:52 +0800

    pdf login

--- 04python网络编程/03ftp/client.py
+++ 04python网络编程/03ftp/client.py
@@ -1,4 +1,5 @@
 import socket
+import getpass
 
 ip_port = ("127.0.0.1",8000)
 
@@ -8,12 +9,48 @@ try:
 except:
     print("服务器连接失败")
 
-while True:
-    path = sk.recv(1024).decode()
-    commend = input("%s>"%path)
-    arr = commend.split()
-    commend = " ".join(arr)
+num = ""
 
-    sk.send(commend.encode())
-    res = sk.recv(1024).decode()
-    print(res)
+# 流程不对
+"""
+客户端登录流程
+cookie
+查看cookie是否存在
+存在 ->跳过登录
+不存在 -> 执行登录
+登录成功-> 设置cookie 跳过登录
+登录失败->执行登录
+
+
+服务端登录流程
+验证用户身份
+是->登录成功 发送 cookie
+不是->登录失败 发送 登录失败
+
+
+
+
+"""
+cookie = "0"
+
+def login():
+    username = input("root@%s's username:"%(ip_port[0]))
+    password = getpass.getpass("root@%s's password:"%(ip_port[0]))
+    con = "%s %s"%(username,password)
+    sk.send(con.encode())
+
+while True:
+    # 如果cookie==0 登录 不等于0 跳过
+    while cookie=="0":
+        login()
+        res = sk.recv(1024).decode() # 登录结果  "ok cookie"  "no message"
+        code,mes = res.split()
+        if code == "ok":
+            cookie = mes
+            print(cookie)
+            break
+        elif code == "no":
+            print(mes)
+
+    print("登录成功")
+
--- 04python网络编程/03ftp/server.py
+++ 04python网络编程/03ftp/server.py
@@ -1,17 +1,42 @@
 import socket,threading
-import os
+import os,os.path,random
+
+root = r"C:\Users\yangd\Desktop\uaif1901"
+
+users = {
+    'ydh':{
+        'password':'123456',
+        'home':r"C:\Users\yangd\Desktop\uaif1901\04python网络编程\03ftp"
+    },
+    'xm':{
+        'password':'123456',
+        'home':r"C:\Users\yangd\Desktop\uaif1901\04python网络编程\03ftp"
+    },
+}
+
+
+
 
 def main(conn,addr):
+    isLogin = False
     while True:
-        os.chdir(r"C:\Users\yangd\Desktop\uaif1901")
-        conn.send(r"C:\Users\yangd\Desktop\uaif1901".encode())
-        commend = conn.recv(1024).decode()
+        # 检测登录
+        while not isLogin:
+            info = conn.recv(1024).decode()
+            username,password = info.split()
+            if username in users:
 
-        if "dir" in commend:
-            arr = os.listdir(os.getcwd())
-            res = "\n".join(arr)
-            print(res)
-            conn.send(res.encode())
+                if users[username]['password']==password:
+                    text = "ok %s"%(random.randint(1000,9999))
+                    conn.send(text.encode())
+                else:
+                    conn.send("no 密码错误".encode())
+            else:
+                conn.send("no 没有此用户".encode())
+        # 执行命令
+
+
+
 
 
 
